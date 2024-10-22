3085eec48764e9aa2aea208615405f1fb16004ad
Nicolas Leon <nicolasaleon05@gmail.com>
2024-10-21 21:25:03 -0300

    Add accelerate sound

--- src/Utils/SoundManager.cpp
+++ src/Utils/SoundManager.cpp
@@ -4,11 +4,16 @@ namespace SoundManager
 {	
 	Music menuMusic;
 	Music gameplayMusic;
+	Sound accelerateSound;
 
 	void Init()
 	{
 		InitAudioDevice();
 		menuMusic = LoadMusicStream("res/Music/Asteroids_Adventure.mp3");
 		gameplayMusic = LoadMusicStream("res/Music/Pixel_Stars.mp3");
+
+		accelerateSound = LoadSound("res/SFX/Accelerate.mp3");
+		SetSoundVolume(accelerateSound, 0.5);
+
 	}
 }
--- src/Utils/SoundManager.h
+++ src/Utils/SoundManager.h
@@ -6,5 +6,6 @@ namespace SoundManager
 	extern Music menuMusic;
 	extern Music gameplayMusic;
 
+	extern Sound accelerateSound;
 	void Init();
 }
