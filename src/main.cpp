88e487b25aa58476215582ae690fcbff401cc993
Nicolas Leon <nicolasaleon05@gmail.com>
2024-10-25 12:38:59 -0300

    Change main location

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
 
+//Mover main a src
+
 //Completar Scene_Tutorial
 //Hacer Scene_Credits
 //Añadir vida al player
@@ -8,6 +10,7 @@
 //Tal vez conviene hacer un sprite.h o algo asi para que tenga el source, destination, scale, rotation
 //Tal vez conviene que en lugar de un vector de enemy sea una list
 //Hacer funcion en utils.h para calcular el angulo de movimiento para las balas, el player y los enemies
+//Agregar tags
 
 int main()
 {
