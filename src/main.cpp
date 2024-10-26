03937d1dba430a1953c9df194719798c0f6268c9
Nicolas Leon <nicolasaleon05@gmail.com>
2024-10-26 15:46:03 -0300

    Update objectives

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,8 @@
 
 //Completar Scene_Tutorial
 //Hacer Scene_Credits
-//Añadir vida al player
-//Añadir disparos
 //Hacer un cpp y h para un text que se use en button
 //Tal vez conviene hacer un sprite.h o algo asi para que tenga el source, destination, scale, rotation
-//Tal vez conviene que en lugar de un vector de enemy sea una list
-//Hacer funcion en utils.h para calcular el angulo de movimiento para las balas, el player y los enemies
 //Agregar tags
 
 int main()
