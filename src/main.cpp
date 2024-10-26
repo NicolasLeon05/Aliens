b48251d0aed4ba1c37a06f07785b5266fef6a52d
Nicolas Leon <nicolasaleon05@gmail.com>
2024-10-26 15:50:33 -0300

    Update objectives

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 
 //Completar Scene_Tutorial
 //Hacer Scene_Credits
+//Hacer Scene_GameOver
+//Hacer Scene_Pause
 //Hacer un cpp y h para un text que se use en button
 //Tal vez conviene hacer un sprite.h o algo asi para que tenga el source, destination, scale, rotation
 //Agregar tags
+//Agregar funcion para respawnear enemigos cuando se destruye uno pequeño (o evaluar otra condicion)
 
 int main()
 {
